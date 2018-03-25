03354a66f0fd770a4424e3cff5a2a4498abcf44c
u16kuma <u16.kuma@gmail.com>
2018-03-25 16:42:19 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# DefUniCo
+
+DefUniCoはDefoldで使われることを想定したコルーチンのライブラリです。  
+DefUniCo is a coroutine library for Defold game engine.  
+Defold上でUnityライクにコルーチンを使用したい場合にとても役に立ちます。  
+The library is useful when you want to write coroutine like Unity3D in Defold.  
+
+# Installation
+
+You can use DefUniCo in your own project by adding this project as a [Defold library dependency](https://www.defold.com/manuals/libraries/).  
+Open your game.project file and in the dependencies field under project add:  
+
+// TODO: zipのパス
+
+Or point to the ZIP file of a [パスを追加する specific DefUniCo release](https://github.com/ぱす/releases).  
+
+# Usage
+
+DefUniCo is very easy to use.  
+All you need to do is to write template code.  
+
+test.script  
+
+```lua
+local defunico = require("defunico.defunico")
+function init(self)
+    defunico.init(self)
+end
+function update(self, dt)
+    self.update_coroutine(dt)
+end
+```
+
+Let's try write coroutine!
+
+```lua
+function init(self)
+    defunico.init(self)
+
+    -- Please write coroutine code under the defunico.init
+    self.start_coroutine(function(self)
+        -- wait one second
+        wait_seconds(1)
+
+        print("One second has passed.")
+    end)
+end
+```
+
+Coroutines can be stopped by adding this code.
+
+```lua
+local co = self.start_coroutine(function() ... end)
+self.stop_coroutine(co)
+```
+
+Coroutine wait some wait_* function.
+
+```lua
+self.start_coroutine(function(self)
+
+    -- wait one second
+    wait_seconds(1)
+
+    -- wait one second
+    wait_msec(1000)
+    
+    -- wait one second (60 fps)
+    wait_frame(60)
+
+    -- wait one second (60 fps)
+    local frame = 0
+    wait_until(function() 
+        frame = frame + 1
+        return frame >= 60
+    end)
+
+    -- wait one second (60 fps)
+    frame = 0
+    wait_while(function()
+        frame = frame + 1
+        return frame <= 60
+    end)
+
+end)
+```
---
+++ defunico/defunico.lua
@@ -0,0 +1,98 @@
+local defunico = {}
+
+local WAIT_TYPE_SECONDS = 1
+local WAIT_TYPE_UNTIL = 2
+local WAIT_TYPE_WHILE = 3
+local WAIT_TYPE_FRAME = 4
+
+function defunico.init(self)
+	self.unico_list = {}
+	self.dt = 0.0
+
+	local unico = {}
+	function unico.new(instance, f)
+		local self = {}
+		local co_ins = coroutine.create(f)
+		local status
+		local wait_type
+		local wait_value
+		local is_waiting = false
+		local time = 0
+		function self:move_next()
+			if (not is_waiting) then
+				status, wait_type, wait_value = coroutine.resume(co_ins, instance)
+				if status then
+					is_waiting = true
+				end
+			end
+
+			if is_waiting then
+				if wait_type == WAIT_TYPE_SECONDS then
+					time = time + instance.dt
+					if time >= wait_value then
+						is_waiting = false
+						time = 0
+					end
+				elseif wait_type == WAIT_TYPE_UNTIL then
+					if wait_value() then
+						is_waiting = false
+					end
+				elseif wait_type == WAIT_TYPE_WHILE then
+					if (not wait_value()) then
+						is_waiting = false
+					end
+				elseif wait_type == WAIT_TYPE_FRAME then
+					wait_value = wait_value - 1
+					if wait_value <= 0 then
+						is_waiting = false
+					end
+				end
+			end
+			
+			return coroutine.status(co_ins)
+		end
+		return self
+	end
+	
+	function self.start_coroutine(f)
+		local co = unico.new(self, f)
+		table.insert(self.unico_list, co)
+		return co
+	end
+	function self.update_coroutine(dt)
+		self.dt = dt
+		for i, co_ins in ipairs(self.unico_list) do
+			if co_ins:move_next() == "dead" then
+				-- remove unico
+				table.remove(self.unico_list, i)
+			end
+		end
+	end
+	function self.stop_coroutine(co)
+		for co_ins in #self.unico_list do
+			if co_ins == co then
+				table.remove(self.unico_list, i)
+			end
+		end
+	end
+end
+
+function wait_seconds(sec)
+	coroutine.yield(WAIT_TYPE_SECONDS, sec)
+end
+function wait_msec(msec)
+	coroutine.yield(WAIT_TYPE_SECONDS, msec / 1000)
+end
+function wait_until(func)
+	coroutine.yield(WAIT_TYPE_UNTIL, func)
+end
+function wait_while(func)
+	coroutine.yield(WAIT_TYPE_WHILE, func)
+end
+function wait_next_frame()
+	coroutine.yield(WAIT_TYPE_FRAME, 1)
+end
+function wait_frame(frame)
+	coroutine.yield(WAIT_TYPE_FRAME, frame)
+end
+return defunico
