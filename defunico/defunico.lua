dd641c1bf019d80371153a86d586989e21c0028c
u16kuma <u16.kuma@gmail.com>
2018-04-03 22:37:18 +0900

    add wait_until_input add wait_until_message

--- defunico/defunico.lua
+++ defunico/defunico.lua
@@ -4,6 +4,8 @@ local WAIT_TYPE_SECONDS = 1
 local WAIT_TYPE_UNTIL = 2
 local WAIT_TYPE_WHILE = 3
 local WAIT_TYPE_FRAME = 4
+local WAIT_TYPE_MESSAGE = 5
+local WAIT_TYPE_INPUT = 6
 
 function defunico.init(self)
 	self.unico_list = {}
@@ -18,7 +20,7 @@ function defunico.init(self)
 		local wait_value
 		local is_waiting = false
 		local time = 0
-		function self:move_next()
+		function self:resume()
 			if (not is_waiting) then
 				status, wait_type, wait_value = coroutine.resume(co_ins, instance)
 				if status then
@@ -27,7 +29,10 @@ function defunico.init(self)
 					print(debug.traceback(co_ins, wait_type))
 				end
 			end
-
+		end
+		function self:update_move_next()
+			self:resume()
+			
 			if is_waiting then
 				if wait_type == WAIT_TYPE_SECONDS then
 					time = time + instance.dt
@@ -53,6 +58,32 @@ function defunico.init(self)
 			
 			return coroutine.status(co_ins)
 		end
+		function self:input_move_next()
+			self:resume()
+
+			if is_waiting then
+				if wait_type == WAIT_TYPE_INPUT then
+					if wait_value(instance.input_pack.action_id, instance.input_pack.action) then
+						is_waiting = false
+					end
+				end
+			end
+
+			return coroutine.status(co_ins)
+		end
+		function self:message_move_next()
+			self:resume()
+
+			if is_waiting then
+				if wait_type == WAIT_TYPE_MESSAGE then
+					if wait_value(instance.message_pack.message_id, instance.message_pack.message, instance.message_pack.sender) then
+						is_waiting = false
+					end
+				end
+			end
+
+			return coroutine.status(co_ins)
+		end
 		return self
 	end
 	
@@ -61,21 +92,47 @@ function defunico.init(self)
 		table.insert(self.unico_list, co)
 		return co
 	end
-	function self.update_coroutine(dt)
-		self.dt = dt
-		for i, co_ins in ipairs(self.unico_list) do
-			if co_ins:move_next() == "dead" then
-				-- remove unico
-				table.remove(self.unico_list, i)
-			end
-		end
-	end
 	function self.stop_coroutine(co)
 		for co_ins in #self.unico_list do
 			if co_ins == co then
 				table.remove(self.unico_list, i)
 			end
 		end
+	end
+	function self.update_coroutine(dt)
+		self.dt = dt
+		for i, co_ins in ipairs(self.unico_list) do
+			if co_ins:update_move_next() == "dead" then
+				-- remove unico
+				table.remove(self.unico_list, i)
+			end
+		end
+	end
+	function self.on_input_coroutine(action_id, action)
+		self.input_pack = { action_id = action_id, action = action }
+		for i, co_ins in ipairs(self.unico_list) do
+			if co_ins:input_move_next() == "dead" then
+				-- remove unico
+				table.remove(self.unico_list, i)
+			end
+		end
+	end
+	function self.on_message_coroutine(message_id, message, sender)
+		self.message_pack = { message_id = message_id, message = message, sender = sender }
+		for i, co_ins in ipairs(self.unico_list) do
+			if co_ins:message_move_next() == "dead" then
+				-- remove unico
+				table.remove(self.unico_list, i)
+			end
+		end
+	end
+	function self.wait_until_input(func)
+		coroutine.yield(WAIT_TYPE_INPUT, func)
+		return self.input_pack
+	end
+	function self.wait_until_message(func)
+		coroutine.yield(WAIT_TYPE_MESSAGE, func)
+		return self.message_pack
 	end
 end
 
